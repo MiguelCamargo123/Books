d0f80b3cd5f20e4ad0a097b8c7d558f5c2cd9d1e
Miguel Camargo <omiguelcamargo@gmail.com>
2026-02-20 16:50:41 -0300

    Loop criado junto com a pergunta perguntando se a pessoa deseja entrar na biblioteca

--- books.py
+++ books.py
@@ -14,7 +14,7 @@ def registarLivro():
     nome = input('Nome do livro: ')
     genero = input('Genero do livro: ')
     autor = input('Autor do livro: ')
-    anoPublicado = int(input('Data de publicação: '))
+    anoPublicado = int(input('Ano de publicação: '))
 
     livro = Livro(nome, genero, autor, anoPublicado)
 
@@ -24,3 +24,22 @@ def verLivros():
     print('Atualmente temos os seguintes livros: ')
     for i, livros in enumerate(biblioteca, start=1):
         print(f'-{i} {livros}')
+
+pergunta = input('Você deseja entrar em nossa biblioteca? (S/N) ').upper()
+
+while pergunta == 'S':
+    print()
+    oqueFazer = input('Você deseja [R]egistrar um livro, [V]er os livros? ').upper()
+
+    if oqueFazer == 'R':
+        try:
+            print()
+            registarLivro()
+            print('Livro registrado com sucesso!')
+        except ValueError:
+            print()
+            print('Por favor, digite um ano, não um texto')
+
+    if oqueFazer == 'V':
+        print()
+        verLivros()
